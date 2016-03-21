a2e26010d341e7c54073ee7239389cc2d77ccf5d
U47 <u47@u47.ca>
2016-03-20 20:06:32 -0600

    Updates to HHKB and Infinity custom layouts.

--- keyboard/hhkb/keymap_u47.c
+++ keyboard/hhkb/keymap_u47.c
@@ -12,39 +12,21 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |-----------------------------------------------------------|
      * |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|    \|
      * |-----------------------------------------------------------|
-     * |Fn29  |  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|Enter   |
+     * |Fn30  |  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|Enter   |
      * |-----------------------------------------------------------|
-     * |Fn30    |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|Fn31  |Fn0|
+     * |LSft    |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|RSft  |Fn0|
      * `-----------------------------------------------------------'
      *       |Alt|Gui  |         SpaceFN       |Gui  |Alt|
      *       `-------------------------------------------'
      */
+    [0] = 
     KEYMAP(ESC, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSLS, GRV,  \
-           FN3, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSPC,       \
-           FN29,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,ENT,             \
-           FN30,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,FN31,FN0,             \
+           FN2, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSPC,       \
+           FN30,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,ENT,             \
+           LSFT,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,RSFT,FN0,             \
                 LALT,LGUI,          FN1,                RGUI,RALT),
 
-    /* Layer 1: Shift Layer
-     * ,-----------------------------------------------------------.
-     * |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |
-     * |-----------------------------------------------------------|
-     * |     |   |   |   |   |   |   |   |   |   |   |   |   |     |
-     * |-----------------------------------------------------------|
-     * |Delete|   |   |   |   |   |   |   |   |   |   |   |        |
-     * |-----------------------------------------------------------|
-     * |        |   |   |   |   |   |   |   |   |   |   |      |   |
-     * `-----------------------------------------------------------'
-     *       |   |     |                       |     |   |
-     *       `-------------------------------------------'
-     */
-    KEYMAP(TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,  \
-           TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,       \
-           DEL, TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,            \
-           TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,            \
-                TRNS,TRNS,          TRNS,               TRNS,TRNS),
-
-    /* Layer 2: HHKB mode (HHKB Fn)
+    /* Layer 1: HHKB mode (HHKB Fn)
      * ,-----------------------------------------------------------.
      * |Pwr| F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12|Ins|Del|
      * |-----------------------------------------------------------|
@@ -56,33 +38,35 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * `-----------------------------------------------------------'
      *       |   |     |                       |     |   |
      *       `-------------------------------------------'
-     */ 
+     */
+    [1] = 
     KEYMAP(PWR, F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12, INS, DEL,   \
            CAPS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,PSCR,SLCK,PAUS, UP, TRNS, BSPC,      \
            TRNS,VOLD,VOLU,MUTE,TRNS,TRNS,PAST,PSLS,HOME,PGUP,LEFT,RGHT,PENT,            \
            TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,PPLS,PMNS,END, PGDN,DOWN,TRNS,TRNS,            \
                 TRNS,TRNS,          SPC,                TRNS,TRNS),
 
-    /* Layer 3: SpaceFN
+    /* Layer 2: SpaceFN
      * ,-----------------------------------------------------------.
-     * |`  | F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12|   |Del|
+     * |Pwr| F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12|   |Del|
      * |-----------------------------------------------------------|
      * |     |   |   |Esc|   |   |   |Hom|Up |End|Psc|Slk|Pau|Ins  |
      * |-----------------------------------------------------------|
-     * |      |   |   |   |   |   |PgU|Lef|Dow|Rig|   |   |        |
+     * |Fn31  |   |   |   |   |   |PgU|Lef|Dow|Rig|   |   |        |
      * |-----------------------------------------------------------|
      * |        |   |   |   |   |Spc|PgD|`  |~  |   |   |      |   |
      * `-----------------------------------------------------------'
      *       |   |     |                       |     |   |
      *       `-------------------------------------------'
      */
-    KEYMAP(GRV, F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12, TRNS,DEL,   \
+    [2] = 
+    KEYMAP(PWR, F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12, TRNS,DEL,   \
            TRNS,TRNS,TRNS,ESC, TRNS,TRNS,TRNS,HOME,UP,  END, PSCR,SLCK,PAUS,INS,        \
-           TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,PGUP,LEFT,DOWN,RGHT,TRNS,TRNS,TRNS,            \
+           FN31,TRNS,TRNS,TRNS,TRNS,TRNS,PGUP,LEFT,DOWN,RGHT,TRNS,TRNS,TRNS,            \
            TRNS,TRNS,TRNS,TRNS,TRNS,SPC, PGDN,GRV, FN2, TRNS,TRNS,TRNS,NO,              \
                 TRNS,TRNS,          TRNS,               TRNS,TRNS),
 
-    /* Layer 4: Virtual numpad
+    /* Layer 3: Virtual numpad
      * ,-----------------------------------------------------------.
      * |   |   |   |   |   |   |   |  7|  8|  9|   |  -|  =|   |   |
      * |-----------------------------------------------------------|
@@ -95,6 +79,7 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      *       |   |     |                       |     |   |
      *       `-------------------------------------------'
      */
+    [3] = 
     KEYMAP(TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,P7,  P8,  P9,  TRNS,PMNS,PEQL,TRNS,TRNS,  \
            TRNS,TRNS,TRNS,ESC, TRNS,TRNS,TRNS,P4,  P5,  P6,  TRNS,PENT,TRNS,TRNS,       \
            TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,PGUP,P1,  P2,  P3,  PPLS,PAST,TRNS,            \
@@ -106,7 +91,8 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /*
  * user defined action function
  */
-enum function_id {
+/*
+ * enum function_id {
     CTRL_SPACE_I,       // Ctrl + Up(SpaceFN) -> PgUp
 };
 
@@ -141,18 +127,17 @@ void action_function(keyrecord_t *record, uint8_t id, uint8_t opt)
             break;
     }
 }
-
+*/
 
 /*
  * Fn action definition
  */
 const uint16_t PROGMEM fn_actions[] = {
-    [0] = ACTION_LAYER_MOMENTARY(2),              // HHKB Fn
-    [1] = ACTION_LAYER_TAP_KEY(3, KC_SPACE),      // SpaceFN
-    [2] = ACTION_MODS_KEY(MOD_LSFT, KC_GRV),      // tilde
-    [3] = ACTION_LAYER_TAP_KEY(4, KC_TAB),        // Virtual numpad
-    [29]= ACTION_MODS_TAP_KEY(MOD_LCTL, KC_BSPC), // LCTRL/BKSP dual role key
-    [30]= ACTION_LAYER_MODS(1, MOD_LSFT),         // LSFT+BKSP -> DEL
-    [31]= ACTION_LAYER_MODS(1, MOD_RSFT)          // RSFT+BKSP -> DEL
+    [0] = ACTION_LAYER_MOMENTARY(1),              // HHKB Fn
+    [1] = ACTION_LAYER_TAP_KEY(2, KC_SPACE),      // SpaceFN
+//    [2] = ACTION_MODS_KEY(MOD_LSFT, KC_GRV),      // tilde
+    [2] = ACTION_LAYER_TAP_KEY(3, KC_TAB),        // Virtual numpad
+    [30]= ACTION_MODS_TAP_KEY(MOD_LCTL, KC_BSPC), // LCTRL/BKSP dual role key
+    [31]= ACTION_MODS_TAP_KEY(MOD_LCTL, KC_DEL),  // LCTRL/Del dual role key
 
 };
--- keyboard/infinity/keymap_u47.c
+++ keyboard/infinity/keymap_u47.c
@@ -5,21 +5,21 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * ,-----------------------------------------------------------.
      * |Esc|  1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =|  `|BSp|
      * |-----------------------------------------------------------|
-     * |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|    \|
+     * |Fn2  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|Backs|
      * |-----------------------------------------------------------|
-     * |Contro|  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|Enter   |
+     * |Fn30  |  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|Enter   |
      * |-----------------------------------------------------------|
-     * |Shift   |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|Shift |Fn0|
+     * |LSft    |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|RSft  |Fn0|
      * |-----------------------------------------------------------'
-     * |     |Gui|Alt  |         Space         |Alt  |Gui|   |     |
+     * |     |Gui|Alt  |          Fn1          |Alt  |Gui|   |     |
      * `-----------------------------------------------------------'
      */
     [0] =
     KEYMAP(ESC, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSLS, GRV, \
-           TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSPC, \
-           LCTL,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,ENT,  \
+           FN2, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSPC, \
+           FN30,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,ENT,  \
            LSFT,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,RSFT,FN0, \
-           NO,  LGUI,LALT,          SPC,                RALT,RGUI,NO, NO),
+           NO,  LALT,LGUI,          FN1,                RGUI,RALT,NO, NO),
 
     /* Layer 1: HHKB mode (HHKB Fn)
      * ,-----------------------------------------------------------.
@@ -27,7 +27,7 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |-----------------------------------------------------------|
      * |Caps |   |   |   |   |   |   |   |Psc|Slk|Pus|Up |   |Backs|
      * |-----------------------------------------------------------|
-     * |      |VoD|VoU|Mut|   |   |  *|  /|Hom|PgU|Lef|Rig|Enter   |
+     * |Fn31  |VoD|VoU|Mut|   |   |  *|  /|Hom|PgU|Lef|Rig|Enter   |
      * |-----------------------------------------------------------|
      * |        |   |   |   |   |   |  +|  -|End|PgD|Dow|      |   |
      * `-----------------------------------------------------------'
@@ -41,13 +41,13 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
            TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,PPLS,PMNS,END, PGDN,DOWN,TRNS,TRNS,            \
            TRNS,TRNS,TRNS,          TRNS,               TRNS,TRNS,TRNS,TRNS),
 
-/* Layer 2: SpaceFN
+    /* Layer 2: SpaceFN
      * ,-----------------------------------------------------------.
      * |Pwr| F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12|   |Del|
      * |-----------------------------------------------------------|
      * |Caps |   |   |Esc|   |   |   |Hom|Up |End|Psc|Slk|Pau|Ins  |
      * |-----------------------------------------------------------|
-     * |      |   |   |   |   |   |PgU|Lef|Dow|Rig|   |   |        |
+     * |Fn31  |   |   |   |   |   |PgU|Lef|Dow|Rig|   |   |        |
      * |-----------------------------------------------------------|
      * |        |   |   |   |   |Spc|PgD|   |   |   |   |      |   |
      * `-----------------------------------------------------------'
@@ -57,13 +57,36 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [2] = 
     KEYMAP(PWR, F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12, TRNS,DEL,   \
            CAPS,TRNS,TRNS,ESC, TRNS,TRNS,TRNS,HOME,UP,  END, PSCR,SLCK,PAUS,INS,        \
-           TRNS,TRNS,TRNS,MUTE,TRNS,TRNS,PGUP,LEFT,DOWN,RGHT,TRNS,TRNS,TRNS,            \
+           FN31,TRNS,TRNS,MUTE,TRNS,TRNS,PGUP,LEFT,DOWN,RGHT,TRNS,TRNS,TRNS,            \
            TRNS,TRNS,TRNS,TRNS,TRNS,SPC, PGDN,TRNS,TRNS,TRNS,TRNS,TRNS,NO,              \
+           TRNS,TRNS,TRNS,          TRNS,               TRNS,TRNS,TRNS,TRNS),
+
+    /* Layer 3: Virtual numpad
+     * ,-----------------------------------------------------------.
+     * |   |   |   |   |   |   |   |  7|  8|  9|   |  -|  =|   |   |
+     * |-----------------------------------------------------------|
+     * |     |   |   |   |   |   |   |  4|  5|  6|   |Ent|   |     |
+     * |-----------------------------------------------------------|
+     * |      |   |   |   |   |   |   |  1|  2|  3|  +|  *|        |
+     * |-----------------------------------------------------------|
+     * |        |   |   |   |   |   |   |  0|   |  .|  /|      |   |
+     * `-----------------------------------------------------------'
+     * |     |   |     |                       |     |   |   |     |
+     * `-----------------------------------------------------------'
+     */
+    [3] = 
+    KEYMAP(TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,P7,  P8,  P9,  TRNS,PMNS,PEQL,TRNS,TRNS,  \
+           TRNS,TRNS,TRNS,ESC, TRNS,TRNS,TRNS,P4,  P5,  P6,  TRNS,PENT,TRNS,TRNS,       \
+           TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,PGUP,P1,  P2,  P3,  PPLS,PAST,TRNS,            \
+           TRNS,TRNS,TRNS,TRNS,TRNS,SPC, PGDN,P0,  TRNS,PDOT,TRNS,TRNS,NO,              \
            TRNS,TRNS,TRNS,          TRNS,               TRNS,TRNS,TRNS,TRNS),
 };
 
 const uint16_t PROGMEM fn_actions[] = {
-    [0] = ACTION_LAYER_MOMENTARY(1),
-    [1] = ACTION_LAYER_TAP_KEY(2, KC_SPACE),
+    [0] = ACTION_LAYER_MOMENTARY(1),              // HHKB Fn
+    [1] = ACTION_LAYER_TAP_KEY(2, KC_SPACE),      // SpaceFN
+    [2] = ACTION_LAYER_TAP_KEY(3, KC_TAB),        // Virtual numpad
+    [30]= ACTION_MODS_TAP_KEY(MOD_LCTL, KC_BSPC), // LCTRL/BKSP dual role key
+    [31]= ACTION_MODS_TAP_KEY(MOD_LCTL, KC_DEL),  // LCTRL/Del dual role key
 };
 
